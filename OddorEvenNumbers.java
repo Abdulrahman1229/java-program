9904e009353c28428f5ac13d5762a6aeb720c8c6
Abdulrahman1229 <usmaana46@gmail.com>
2024-12-21 21:57:00 +0530

    Add files via upload

---
+++ OddorEvenNumbers.java
@@ -0,0 +1,19 @@
+
+
+
+
+public class OddorEvenNumbers {
+	public static void main (String args[]) {
+	Scanner sc =new Scanner(System.in);
+		System.out.println("Enter the number:");
+		int num=sc.nextInt();
+		if(num%2==0) {
+		System.out.println("Given number is Even");
+	}
+	else  {
+		System.out.println("Given number is Odd");
+	}
+	}
+}
+
+	
